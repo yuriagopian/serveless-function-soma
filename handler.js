f05130a59bb28427820f75c1d7ed3313d78f81eb
dev3 <yuri.agopian@hotmail.com>
2020-07-03 22:37:29 -0300

    adcionando subtracao

--- handler.js
+++ handler.js
@@ -6,5 +6,12 @@ function soma({ a = 0, b = 0 }) {
   return response;
 }
 
+function subtracao({ a = 0, b = 0 }) {
+  const response = { resultado: Number(a) - Number(b) };
+  console.log('log', response);
+  return response;
+}
+
 
 exports.soma = soma;
+exports.subtracao = subtracao;
