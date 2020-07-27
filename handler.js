daf86797c50331d0c45834d6014a34b59266bc90
dev3 <yuri.agopian@hotmail.com>
2020-07-27 15:09:17 -0300

    first commit

--- handler.js
+++ handler.js
@@ -7,7 +7,7 @@ function soma({ a = 0, b = 0 }) {
 }
 
 function subtracao({ a = 0, b = 0 }) {
-  const response = { resultado: Number(a) - Number(b) };
+  const response = { resultado: Number(a) + Number(b) };
   console.log('log', response);
   return response;
 }
